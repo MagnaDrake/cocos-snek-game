2f323d6dec9cc2bf810a69208ed16c5204a3e491
unknown <ricky.sunartio@shopee.com>
2021-09-22 00:20:13 +0700

    Remove utils that seem to be too project specific / already exist in cocos

--- assets/script/lib/util/checkExist.ts
+++
@@ -1,9 +0,0 @@
-export function checkExist<T>(
-  data: T | undefined | null,
-  errMessage: string = "Error on checkExist. data is undefined"
-): T {
-  if (!data) {
-    throw new Error(errMessage);
-  }
-  return data;
-}
