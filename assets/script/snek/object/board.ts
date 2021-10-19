4875c471eab3e716c982ed8059e94c6448cd9174
Yudha Satria <yudha.putra@shopee.com>
2021-10-19 14:15:19 +0700

    add board generator

--- assets/script/snek/object/board.ts
+++ assets/script/snek/object/board.ts
@@ -1,7 +1,7 @@
 import { _decorator, Component, Node, instantiate, math, v2 } from "cc";
 import { BaseSprite } from "../../lib/sprite/baseSprite";
 import { getTileType, TILE_TYPE } from "../enum/tile";
-import { ITile } from "../interface/ITile";
+import { ITile, ITileSprite } from "../interface/ITile";
 //import { AppleSprite } from "../sprite/appleSprite";
 import { FloorSprite } from "../sprite/floorSprite";
 import { WallSprite } from "../sprite/wallSprite";
@@ -11,10 +11,10 @@ const { ccclass, property } = _decorator;
 @ccclass("Board")
 export class Board extends Component {
   @property(FloorSprite)
-  public readonly tileSprite?: BaseSprite;
+  public readonly floorSprite?: FloorSprite;
 
   @property(WallSprite)
-  public readonly wallSprite?: BaseSprite;
+  public readonly wallSprite?: WallSprite;
 
   @property(Node)
   public readonly tileNode?: Node;
@@ -30,5 +30,78 @@ export class Board extends Component {
 
   constructor() {
     super("Board");
+  }
+
+  public generateBoardFromLevelConfig(levelConfig: Array<Array<number>>) {
+    this.board = this.getBoardFromLevelConfig(levelConfig);
+  }
+
+  private getBoardFromLevelConfig(levelConfig: Array<Array<number>>) {
+    return levelConfig.map((row, rowIndex) => {
+      return row.map((tile, colIndex) => {
+        return {
+          value: tile,
+          index: v2(colIndex, rowIndex),
+        } as ITile;
+      });
+    });
+  }
+
+  private getTile(colIndex: number, rowIndex: number) {
+    const row = this.board[rowIndex];
+    if (row) {
+      return row[colIndex];
+    }
+    return undefined;
+  }
+
+  /*
+  returns the sprite 'prefab' script assigned
+  */
+  private getTileSprite(tile: ITile) {
+    const tileType = getTileType(tile.value);
+    switch (tileType) {
+      case TILE_TYPE.WALL: {
+        return this.wallSprite;
+      }
+
+      default: {
+        return this.floorSprite;
+      }
+    }
+  }
+
+  public getTilePosition(colIndex: number, rowIndex: number) {
+    const { tileSize } = this;
+    return {
+      x: colIndex * tileSize,
+      y: -rowIndex * tileSize,
+    };
+  }
+
+  private assignNodeToTile(colIndex: number, rowIndex: number, node: Node) {
+    const tile = this.getTile(colIndex, rowIndex);
+    if (tile) {
+      tile.node = node;
+    }
+  }
+
+  public generateBoardSprites() {
+    this.board.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
+        const tileSprite = this.getTileSprite(tile);
+        if (tileSprite) {
+          const { x, y } = this.getTilePosition(colIndex, rowIndex);
+          const tile = instantiate(tileSprite);
+          tile.node.setParent(this.tileNode || this.node);
+          tile.node.setPosition(x, y);
+          tile.node.active = true;
+
+          tile.adjustTexture((colIndex + rowIndex) % 2 === 0);
+
+          this.assignNodeToTile(colIndex, rowIndex, tile.node);
+        }
+      });
+    });
   }
 }
